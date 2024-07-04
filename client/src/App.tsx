b344a9150e31f01693c524a49c2c9900df6e102d
Naimish Bhuva <naimishbbhuva@gmail.com>
2024-07-04 16:59:29 +0530

    ci/cd: routing added

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,27 @@
-import './App.css'
+import './App.css';
+
+// react router dom imports
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+// pages imports
+import Navbar from "./components/navbar/navbar";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Navbar />,
+  },
+  {
+    path: "/test",
+    element: <div>Test</div>,
+  }
+]);
 
 function App() {
 
   return (
-    <>
-     hello furniro
-    </>
+    <RouterProvider router={router} />
   )
 }
 
-export default App
+export default App;
