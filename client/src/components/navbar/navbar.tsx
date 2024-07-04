2d2ba62c38fb06089dc8aa1250924c6223acedb5
Naimish Bhuva <naimishbbhuva@gmail.com>
2024-07-04 16:59:58 +0530

    ci/cd: navbar component added

---
+++ client/src/components/navbar/navbar.tsx
@@ -0,0 +1,12 @@
+import './navbar.scss';
+
+function Navbar() {
+  return (
+    <div className='navbar'>
+        navbar
+        <div className="logo">this is logo</div>
+    </div>
+  )
+}
+
+export default Navbar;
