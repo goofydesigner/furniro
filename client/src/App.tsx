1d999b066cb24edbcf6bdac38697adc5e96a9a9a
Naimish Bhuva <naimishbbhuva@gmail.com>
2024-07-09 21:58:17 +0530

    style: prettier formatting

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,27 +1,34 @@
 import './App.css';
 
 // react router dom imports
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 // pages imports
-import Navbar from "./components/navbar/navbar";
+import Navbar from './components/navbar/navbar';
+import Signin from './pages/signin/signin';
+import Signup from './pages/signup/signup';
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: '/',
     element: <Navbar />,
   },
   {
-    path: "/test",
+    path: '/test',
     element: <div>Test</div>,
-  }
+  },
+  {
+    path: '/signin',
+    element: <Signin />,
+  },
+  {
+    path: '/signup',
+    element: <Signup />,
+  },
 ]);
 
 function App() {
-
-  return (
-    <RouterProvider router={router} />
-  )
+  return <RouterProvider router={router} />;
 }
 
 export default App;
