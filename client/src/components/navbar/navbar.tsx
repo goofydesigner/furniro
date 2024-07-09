e090d4d581a621dbae4ca129f53957232c234cad
Naimish Bhuva <naimishbbhuva@gmail.com>
2024-07-09 23:16:26 +0530

    styles: linting upadtes

--- client/src/components/navbar/navbar.tsx
+++ client/src/components/navbar/navbar.tsx
@@ -1,51 +1,66 @@
 import './navbar.scss';
 import logo from '../../assets/logo.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSearch, faShoppingCart, faHeart, faUser } from '@fortawesome/free-solid-svg-icons'; 
+import { faSearch, faShoppingCart, faHeart, faUser } from '@fortawesome/free-solid-svg-icons';
 
 function Navbar() {
   return (
     <div>
       <nav className="navbar navbar-expand-lg bg-body-tertiary">
         <div className="container">
-
           <a className="navbar-brand" href="#">
             <img src={logo} width="50" height="32" alt="Logo" /> Furniro
           </a>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarTogglerDemo02"
+            aria-controls="navbarTogglerDemo02"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
             <span className="navbar-toggler-icon" />
           </button>
 
           <div className="collapse navbar-collapse justify-content-center" id="navbarTogglerDemo02">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0 mx-auto">
               <li className="nav-item">
-                <a className="nav-link" aria-current="page" href="#">Home</a>
+                <a className="nav-link" aria-current="page" href="#">
+                  Home
+                </a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#">Shop</a>
+                <a className="nav-link" href="#">
+                  Shop
+                </a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#">About</a>
+                <a className="nav-link" href="#">
+                  About
+                </a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#">Contact</a>
+                <a className="nav-link" href="#">
+                  Contact
+                </a>
               </li>
             </ul>
             <div className="d-flex gap-4">
-            <div className="nav-icon">
-              <FontAwesomeIcon icon={faSearch} />
-            </div>
-            <div className="nav-icon">
-              <FontAwesomeIcon icon={faShoppingCart} />
-            </div>
-            <div className="nav-icon">
-              <FontAwesomeIcon icon={faHeart} />
-            </div>
-            <div className="nav-icon">
-              <FontAwesomeIcon icon={faUser} />
+              <div className="nav-icon">
+                <FontAwesomeIcon icon={faSearch} />
+              </div>
+              <div className="nav-icon">
+                <FontAwesomeIcon icon={faShoppingCart} />
+              </div>
+              <div className="nav-icon">
+                <FontAwesomeIcon icon={faHeart} />
+              </div>
+              <div className="nav-icon">
+                <FontAwesomeIcon icon={faUser} />
+              </div>
             </div>
           </div>
-          </div>          
         </div>
       </nav>
     </div>
