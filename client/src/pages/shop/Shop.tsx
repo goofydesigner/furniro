71e6b3ffa1813641ee3e5ac1482bc7bea51d6a8a
AniketBhatti <aniket.imscit20@gmail.com>
2024-07-31 19:03:32 +0530

    chore: Make Component of Furniro's Promise

--- client/src/pages/shop/Shop.tsx
+++ client/src/pages/shop/Shop.tsx
@@ -15,11 +15,7 @@ import Footer from '../../components/footer/Footer';
 import filtering from '../../assets/Icons/Filtering.png'
 import gridRound from '../../assets/Icons/Grid-round.png'
 import viewList from '../../assets/Icons/View-list.png'
-
-import trophy from '../../assets/Icons/Trophy.png'
-import tick from '../../assets/Icons/Tick.png'
-import support from '../../assets/Icons/Support.png'
-import call from '../../assets/Icons/Call.png'
+import Promise from '../../components/our_promise/Promise';
 
 
 
@@ -318,47 +314,8 @@ const Shop = () => {
 
             </div>
 
-
-            <div className="promise">
-                <div className="quality">
-                    <img src={trophy} alt="" style={{width:'3rem', height:'3rem'}} />
-
-                    <div>
-                        <h3>High Quality</h3>
-                        <span>Crafted from top materials</span>
-                    </div>
-                </div>
-
-                <div className="quality">
-                    <img src={tick} alt="" style={{width:'3rem', height:'3rem'}} />
-
-                    <div>
-                        <h3>Warranty Protection</h3>
-                        <span>Over 2 years</span>
-                    </div>
-                </div>
-
-                <div className="quality">
-                    <img src={support} alt="" style={{width:'3rem', height:'3rem'}} />
-                    <div>
-
-                        <h3>Free Shipping</h3>
-                        <span>Order over 150 $</span>
-                    </div>
-                </div>
-
-                <div className="quality">
-                    <img src={call} alt="" style={{width:'3rem', height:'3rem'}} />
-                    <div>
-
-                        <h3>24 / 7 Support</h3>
-                        <span>Dedicated support</span>
-                    </div>
-                </div>
-            </div>
-
-
-
+            <Promise />
+            
             <Footer />
 
 
