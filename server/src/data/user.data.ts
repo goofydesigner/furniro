a7c8ef43ba0dd28fed2596144040caf27425e15c
naimish73 <naimishbbhuva@gmail.com>
2024-12-01 14:35:52 +0530

    "feat: added admin access in models"

--- server/src/data/user.data.ts
+++ server/src/data/user.data.ts
@@ -4,17 +4,20 @@ const users = [
     {
         name: 'test',
         email: 'test@email.com',
-        password: bcrypt.hashSync('test', 10) 
+        password: bcrypt.hashSync('test', 10),
+        isAdmin: true
     },
     {
         name: 'test1',
         email: 'test1@email.com',
-        password: bcrypt.hashSync('test1', 10) 
+        password: bcrypt.hashSync('test1', 10),
+        isAdmin: false
     },
     {
         name: 'test2',
         email: 'test2@email.com',
-        password: bcrypt.hashSync('test2', 10) 
+        password: bcrypt.hashSync('test2', 10),
+        isAdmin: false
     },
 ]
 
