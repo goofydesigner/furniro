2a5266229069e70684a56a4d398effeb0815ed47
naimish73 <naimishbbhuva@gmail.com>
2024-12-01 14:37:20 +0530

    ci: added middleware for auth using token

---
+++ server/src/middleware/auth.middleware.ts
@@ -0,0 +1,31 @@
+import AsyncHandler from 'express-async-handler'
+import { Request, Response, NextFunction} from 'express'
+import User from '../models/user.model'
+import jwt from 'jsonwebtoken'
+
+const protect = AsyncHandler(async (req: Request, res: Response, next: NextFunction) => {
+    let token
+    if(req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+        try {
+            token = req.headers.authorization.split(" ")[1]
+            const decodedToken = jwt.verify(token, process.env.JWT_SECRET_KEY)
+            
+            req.user = await User.findById(decodedToken.id).select('-password')
+            if (!req.user) {
+                res.status(404);
+                throw new Error('User not found');
+            }
+            
+            next()
+        } catch(err) {
+            console.log('Error in auth.middleware: ', err)
+        }
+    }
+
+    if(!token) {
+        res.status(401)
+        throw new Error('Not authorized user...')
+    }
+})
+
+export default protect
